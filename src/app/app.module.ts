bc00cdd5373da6b64c84bdde67cb057054f3ea96
Carlos112233 <carlos.araiza@upam.edu.mx>
2021-12-22 10:23:27 -0600

    Pruebas

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { HeaderComponent } from './plantillas/header/header.component';
+import { FooterComponent } from './plantillas/footer/footer.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    XyzComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NuevoComponent,
+    EditarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
